7896b07aa82f9fb561642ca70ac6488a0d913e48
jaeyow <jaeyow@gmail.com>
2022-06-11 18:15:13 +1000

    try metaflow fan out functionality

--- 01-data-collection-flow.py
+++ 01-data-collection-flow.py
@@ -1,6 +1,5 @@
 from metaflow import FlowSpec, step
 
-
 class DataCollectionFlow(FlowSpec):
     """
     DataCollectionFlow is a flow that runs the data collection process.
@@ -13,15 +12,29 @@ class DataCollectionFlow(FlowSpec):
         is the first step in the flow.
         """
         print("DataCollectionFlow...")
-        self.next(self.data_collection_step_one)
+        self.guests = ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"]
+        self.next(self.data_collection_step_one, foreach="guests")
 
     @step
     def data_collection_step_one(self):
         """
         data_collection_step_one step
         """
-        guest = 'Jose'
-        print(f'DataCollectionFlow says: Hi {guest}!')
+        import time
+        self.guest = self.input
+        time.sleep(1)
+
+        self.next(self.join)
+
+    @step
+    def join(self, inputs):
+        """
+        Join our parallel branches and merge results into a dictionary.
+
+        """
+        for inp in inputs:
+            print(f'DataCollectionFlow says: Hi {inp.guest}!')
+
         self.next(self.end)
 
     @step
